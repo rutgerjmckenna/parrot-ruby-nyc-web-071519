395158908009fc1b21adc1dced7a2fe421d95def
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-15 21:05:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(whatcha_gotta_say = "Squawk!")
-  whatcha_gotta_say = "Pretty bird!"
   puts whatcha_gotta_say
   return whatcha_gotta_say
 end
